9ba956ecf835cef95d08b62fe4d98cde96e18268
张孟杰 <zhangskills@gmail.com>
2019-11-26 13:27:26 +0800

    spring-boot-starter-parent 2.2.1.RELEASE

--- src/main/java/my/auditor/SnowflakeId.java
+++ src/main/java/my/auditor/SnowflakeId.java
@@ -2,16 +2,17 @@ package my.auditor;
 
 import lombok.NoArgsConstructor;
 import my.utils.SnowFlake;
-import org.hibernate.engine.spi.SessionImplementor;
+import org.hibernate.HibernateException;
+import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.hibernate.id.IdentifierGenerator;
 
 import java.io.Serializable;
 
 @NoArgsConstructor
 public class SnowflakeId implements IdentifierGenerator {
-
+    
     @Override
-    public Serializable generate(SessionImplementor s, Object obj) {
+    public Serializable generate(SharedSessionContractImplementor sharedSessionContractImplementor, Object o) throws HibernateException {
         return SnowFlake.getInstance().nextId();
     }
 }
