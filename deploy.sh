5bed5de23b77e775edec4c874a2332f4a6d56b17
jack <zhangskills@gmail.com>
2019-11-27 11:44:03 +0800

    deploy.sh -Xms 128m -Xmx 512m

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,9 @@ git pull
 
 mvn clean package -DskipTests=true dockerfile:build
 
-docker run --rm --name daily \
-  -it -v /etc/localtime:/etc/localtime:ro \
+docker run --rm --name daily -it \
+  -e JAVA_OPTS='-Xms128m -Xmx512m -Dfile.encoding=UTF-8' \
+  -v /etc/localtime:/etc/localtime:ro \
   -v /etc/timezone:/etc/timezone:ro \
   -p 192.168.0.33:8080:8080
   daily:1.0-SNAPSHOT
