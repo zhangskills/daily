c3ad3506020c1d906d2f749fff7d9c4fba498210
jack <zhangskills@gmail.com>
2019-11-27 12:03:39 +0800

    deploy.sh 修正

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ docker run --rm --name daily -it \
   -e JAVA_OPTS='-Xms128m -Xmx512m -Dfile.encoding=UTF-8' \
   -v /etc/localtime:/etc/localtime:ro \
   -v /etc/timezone:/etc/timezone:ro \
-  -p 192.168.0.33:8080:8080
+  -p 192.168.0.33:8080:8080 \
   daily:1.0-SNAPSHOT
