e757ff2bbc1269b332a74aeea28cff3924b490d1
张孟杰 <zhangskills@gmail.com>
2020-11-18 09:38:38 +0800

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@ git pull
 
 mvn clean package -DskipTests=true dockerfile:build
 
-docker run --rm --name daily -it \
-  -e JAVA_OPTS='-Xms128m -Xmx512m -Dfile.encoding=UTF-8' \
+docker run -d --name daily -it \
+  -e JAVA_OPTS='-Xmx256m -Dfile.encoding=UTF-8' \
   -v /etc/localtime:/etc/localtime:ro \
   -v /etc/timezone:/etc/timezone:ro \
   -p 192.168.0.33:8080:8080 \
