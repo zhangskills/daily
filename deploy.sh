1bad1b4ee13d56229e31d43e087c2fdc4d85d2d4
张孟杰 <zhangskills@gmail.com>
2021-01-29 14:04:43 +0800

    pom.xml war

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,4 @@ docker run -d --name daily -it \
   -v /etc/localtime:/etc/localtime:ro \
   -v /etc/timezone:/etc/timezone:ro \
   -p 192.168.0.33:8080:8080 \
-  daily:1.0-SNAPSHOT
+  daily:1.1-SNAPSHOT
