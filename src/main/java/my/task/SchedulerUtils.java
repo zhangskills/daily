61250c009da8eb561cd27ffde7a0c98f129327d8
zmj <zmj@dd.com>
2022-01-22 13:54:56 +0800

    quartz 线程数设置为1

--- src/main/java/my/task/SchedulerUtils.java
+++ src/main/java/my/task/SchedulerUtils.java
@@ -1,18 +1,30 @@
 package my.task;
 
-import lombok.extern.slf4j.Slf4j;
-import org.quartz.*;
-import org.quartz.impl.StdSchedulerFactory;
-
 import static org.quartz.JobBuilder.newJob;
 import static org.quartz.TriggerBuilder.newTrigger;
+
+import java.util.Properties;
+
+import org.quartz.CronScheduleBuilder;
+import org.quartz.Job;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.Trigger;
+import org.quartz.impl.StdSchedulerFactory;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class SchedulerUtils {
 
-    private Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+    private Scheduler scheduler;
 
     public SchedulerUtils() throws SchedulerException {
+        Properties props = new Properties();
+        props.setProperty("org.quartz.threadPool.threadCount", "1");
+        StdSchedulerFactory factory = new StdSchedulerFactory(props);
+        scheduler = factory.getScheduler();
     }
 
     public void addJob(String jobName, String triggerName, String cronExpression, Class<? extends Job> jobClass)
@@ -33,7 +45,7 @@ public class SchedulerUtils {
 
     public void run() throws SchedulerException {
         addJob("j1", "t1", "0 0 7 * * ?", DailyAbideWithMeJob.class);
-//        addJob("j2", "t2", "0 0 14 * * ?", JijinJob.class);
+        // addJob("j2", "t2", "0 0 14 * * ?", JijinJob.class);
 
         scheduler.start();
         log.warn("quartz 启动");
@@ -45,4 +57,3 @@ public class SchedulerUtils {
     }
 
 }
-
